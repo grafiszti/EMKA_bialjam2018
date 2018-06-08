6869d7e27aeb8d850ed8ae84251414b95bfd319d
Mateusz Choinski <mchoinski@ownedoutcomes.com>
2018-06-08 18:36:37 +0200

    added player1, new input actions, player can putting spikes

--- player/player.gd
+++ player/player.gd
@@ -1,22 +1,44 @@
 extends KinematicBody2D
 
-const WALK_SPEED = 200
+var spike = preload("spike.tscn")
+
+export var WALK_SPEED = 200
+export var max_spikes = 3
+export var player_number = "1"
+
 var velocity = Vector2()
 
+var player_spikes
+var carying_spike
+
 func _ready():
-	pass
+	player_spikes = get_node("/root/level/player_spikes" + player_number)
+	carying_spike = get_node("carying_spike")
 	
 func _physics_process(delta):
-	if Input.is_action_pressed("ui_left"):
+	if Input.is_action_pressed("ui_left" + player_number):
 		velocity.x = -WALK_SPEED
-	elif Input.is_action_pressed("ui_right"):
+	if Input.is_action_pressed("ui_right" + player_number):
 		velocity.x = WALK_SPEED
-	elif Input.is_action_pressed("ui_up"):
+	if Input.is_action_pressed("ui_up" + player_number):
 		velocity.y = -WALK_SPEED
-	elif Input.is_action_pressed("ui_down"):
+	if Input.is_action_pressed("ui_down" + player_number):
 		velocity.y = WALK_SPEED
-	else:
-		velocity.x = 0
+	if !Input.is_action_pressed("ui_down" + player_number) && !Input.is_action_pressed("ui_up"+ player_number):
 		velocity.y = 0
+	if !Input.is_action_pressed("ui_left"+ player_number) && !Input.is_action_pressed("ui_right"+ player_number):
+		velocity.x = 0
 		
+	if Input.is_action_just_pressed("ui_set_spike" + player_number):
+		if max_spikes > 0:
+			player_spikes.add_child(build_spike())
+			max_spikes = max_spikes - 1
+			if max_spikes == 0:
+				carying_spike.hide()
+			
 	move_and_slide(velocity, Vector2(0, -1))
+
+func build_spike():
+	var new_spike = spike.instance()
+	new_spike.position = self.global_position + Vector2(35, 0)
+	return new_spike
